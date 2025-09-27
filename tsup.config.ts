d0e9d591c542b1902fb7afd428dc79d61e22199d
kolostring <yoerodriguezjunior@gmail.com>
2025-09-27 02:40:19 -0400

    fix: tsup not building into separate cjs and esm folders

--- tsup.config.ts
+++ tsup.config.ts
@@ -9,4 +9,12 @@ export default defineConfig({
   clean: true,
   target: "es2020",
   splitting: false, // no code-splitting needed for Node library
+  outDir: "dist", // base folder
+  esbuildOptions: (options, ctx) => {
+    if (ctx.format === "cjs") {
+      options.outdir = "dist/cjs"; // CJS goes here
+    } else if (ctx.format === "esm") {
+      options.outdir = "dist/esm"; // ESM goes here
+    }
+  },
 });
